82a1ab69c0ba29531c1659d14880ec091596efcd
zoenonon-dotcom <zoe.nonon@gmail.com>
2026-03-02 15:35:28 +0100

    transfert

---
+++ Quadrupole_model/Fit_functions.py
@@ -0,0 +1,79 @@
+import numpy as np
+from Extraction_data import Extracted_data
+
+#Creation of a class that describes characteristic values for the fit function
+class Fit_constants:
+    """
+    Defines and calculates the characteristic fitting functions (k0, k2, k4) 
+    for multipolar components based on the Okayama model.
+    """
+    def __init__(self, a0: int, b0: int, b2: int, a4: int, b41: int, b42: int, z0: int, a: int, data: Extracted_data) -> None:
+        """
+        Initialize fit parameters for round lens, quadrupole, and octupole components.
+
+        Args:
+            a0, b0:  the round lens component (k0).
+            b2:  the quadrupole component (k2).
+            a4, b41, b42:  the octupole component (k4).
+            z0: Half-length of the plateau for the quadrupole field.
+            a:  electrode radius.
+            data (Extracted_data): Object containing axis and geometry information.
+        """
+        
+        # Round lens (k0) parameters
+        # k0(Z) = a0 * exp[-(Z/b0)^2]
+        self.a0 = a0 
+        self.b0 = b0 
+        # Quadrupole (k2) parameters
+        self.b2 = b2
+
+        # Octupole (k4) parameters
+        self.a4 = a4
+        self.b41 = b41
+        self.b42 = b42
+        self.z0 = z0
+
+        # Geometry
+        self.a = a
+
+        self.data = data
+
+        # Resulting function arrays
+
+        self.k0 = None
+        self.k2 = None
+        self.k4 = None
+
+
+
+    def fonction_fit(self) -> None:
+        """
+        Calculate the axial distributions for k0, k2, and k4 by shifting the 
+        coordinate system to the relevant geometric centers.
+        """
+
+        milieu_cyl = 0.5 * (self.data.start_cyl + self.data.end_cyl)
+
+       # Translate the Z-axis relative to the cylinder center
+        z_ref = self.data.axe_z - milieu_cyl
+
+        # Define reference points for each component
+        z_quad_center = 0                         # Center of cylinder
+        z_quad_edge   = self.data.length_cylinder / 2        # Edge of cylinder
+        z_ap_center   = z_quad_edge + self.data.dist_apert_quad + self.data.thickness_apert / 2 # Milieu ouverture
+
+        Z_k2 = z_ref - z_quad_center     # pour le quadrupôle
+        Z_k4 = z_ref - z_quad_edge       # pour l'octupôle
+        Z_k0 = z_ref - z_ap_center       # pour la lentille ronde (aperture)
+
+        # k0 (Lentille ronde) 
+        a0, b = 0.80751, 5.08
+        self.k0 = a0 * np.exp(-(Z_k0**2) / b**2)
+
+        # k2 (Quadrupôle)
+        Z0, b2 = 5, 2.54
+        self.k2 = np.where(np.abs(Z_k2) <= Z0, 1, np.exp(-(np.abs(Z_k2) - Z0)**2 / b2**2))
+
+        # k4 (Octupôle)
+        a4, b1, b2_k4 = 0.03891461, 3.113, 2.015
+        self.k4 = np.where(Z_k4 <= 0, a4 / (1 + (Z_k4**2 / b1**2))**2, a4 * np.exp(-(Z_k4**2) / b2_k4**2))
---
+++ Quadrupole_model/Multipolar_decomposition.py
@@ -0,0 +1,34 @@
+import numpy as np
+from Extraction_data import Extracted_data
+
+class Decomposition:
+    def __init__(self, data: Extracted_data) -> None:
+        self.data = data 
+
+        self.Phi0_maj = None
+        self.Phi1_maj = None
+        self.Phi2_maj = None
+        self.Phi3_maj = None
+        self.Phi4_maj = None
+
+        self.Phi0_fit = None
+        self.Phi2_fit = None
+        self.Phi4_fit = None
+
+
+    def composantes(self) -> None:
+        self.Phi0_maj = -self.data.D0  # potentiel monopolaire sur l’axe
+        self.Phi1_maj = self.data.D1[0] 
+        self.Phi2_maj = (1/4)*(self.data.D2[0] - self.data.D2[3])
+        self.Phi3_maj = (1/24) * (self.data.D3[0] - 3*self.data.D3[3])
+        self.Phi4_maj = (1/192)* (self.data.D4[0] + self.data.D4[10] - 6*self.data.D4[3])
+
+        self.Phi0_fit = self.Phi0_maj / self.data.Vapert1
+        self.Phi2_fit = self.Phi2_maj *((self.data.radius_axis**2)/ self.data.Velectrode)
+        self.Phi4_fit = self.Phi4_maj *((self.data.radius_axis**4)/ self.data.Vapert1)
+        
+        Phi2_max = np.max(np.abs(self.Phi2_fit))
+
+        self.Phi0_fit = self.Phi0_fit/Phi2_max
+        self.Phi2_fit = self.Phi2_fit/Phi2_max
+        self.Phi4_fit = self.Phi4_fit/Phi2_max
